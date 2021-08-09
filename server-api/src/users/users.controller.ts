d949b8a2221775bfc29ec8ec2921e6f9ff0797b8
Netanel <ne.zaffran@gmail.com>
2021-08-09 22:33:43 +0300

    update controller

--- server-api/src/users/users.controller.ts
+++ server-api/src/users/users.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get()
-  GetAll() {
-    return 'all user';
-  }
+  FinAll() {}
+
+  @Get('/:id')
+  FindOne() {}
+
+  @Post()
+  Create() {}
+
+  @Patch()
+  UpdateOne() {}
+
+  @Delete()
+  DeleteOne() {}
 }
