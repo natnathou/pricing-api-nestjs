3949382c29eebd386a94dbfa74961b23bdcddf2d
natnathou <ne.zaffran@gmail.com>
2021-08-15 21:24:50 +0300

    auth service is created

--- server-api/src/app.module.ts
+++ server-api/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entities';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { User } from './entities/user.entities';
       synchronize: true,
     }),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
